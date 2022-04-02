7bc6bbe38d0a2e1eb873b7770e3f2c8cc392a0e6
Maxim Bekoev <bekoev.maxim@gmail.com>
2022-04-02 13:37:35 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -6,41 +6,32 @@
 
 Nonogram ReadFromFile(const std::string &path) {
     std::ifstream fin(path, std::ios::in);
-    int height, width, border_height, border_width;
-    fin >> height >> width >> border_height >> border_width;
+    size_t height = 0, width = 0;
+    fin >> height >> width;
 
     std::vector<std::vector<int>> rows(height, std::vector<int>(0));
     std::vector<std::vector<int>> cols(width, std::vector<int>(0));
 
-    std::string paddings;
-    getline(fin, paddings);
-    getline(fin, paddings);
-    for (size_t i = 0; i < border_height; ++i) {
-        for (size_t j = 0; j < width; ++j) {
-            std::string potential_number;
-            getline(fin, potential_number);
-            int number = 0;
-            try {
-                number = stoi(potential_number);
-            } catch (const std::invalid_argument &) {}
-            if (number > 0) {
-                cols[j].push_back(number);
-            }
+    for (size_t i = 0; i < width; ++i) {
+        size_t string_len = 0;
+        fin >> string_len;
+        for (size_t j = 0; j < string_len; ++j) {
+            int number;
+            fin >> number;
+            cols[i].push_back(number);
         }
     }
+
     for (size_t i = 0; i < height; ++i) {
-        for (size_t j = 0; j < border_width; ++j) {
-            std::string potential_number;
-            getline(fin, potential_number);
-            int number = 0;
-            try {
-                number = stoi(potential_number);
-            } catch (const std::invalid_argument &) {}
-            if (number > 0) {
-                rows[i].push_back(number);
-            }
+        size_t string_len = 0;
+        fin >> string_len;
+        for (size_t j = 0; j < string_len; ++j) {
+            int number;
+            fin >> number;
+            rows[i].push_back(number);
         }
     }
+
     return {Nonogram(std::move(rows), std::move(cols), width, height)};
 }
 
